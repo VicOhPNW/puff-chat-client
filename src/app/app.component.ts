e0703cb74ec96e350f69f5d8d15ff1e37b51b00c
Hyung Lee <hyungnaelee@gmail.com>
2018-11-06 12:41:03 -0800

    Add firebase email/pw authentication.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
-
+import { AuthenticationService } from './authentication.service';
+import * as firebase from "firebase";
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthenticationService]
 })
 export class AppComponent {
-  title = 'puff-chat';  
+  title = 'puff-chat';
+
+  user;
+  userName: string;
+
+  constructor(public authService: AuthenticationService) {
+    this.authService.user.subscribe(user => {
+      console.log(user);
+    });
+  }
+
+  ngDoCheck() {
+    this.user = firebase.auth().currentUser;
+    console.log(this.user);
+  }
+
+  login(email, password) {
+    this.authService.login(email, password);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  updateDisplayName(newName) {
+    this.authService.updateDisplayName(newName);
+  }
 }
---
+++ src/app/authentication.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class AuthenticationService {
+  user: Observable<firebase.User>;
+
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  // login(email, password) {
+  //   this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  //   console.log(this.user);
+  // }
+
+  login(email, password) {
+    this.afAuth.auth.signInWithEmailAndPassword(email, password));
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  updateDisplayName(newName) {
+    let user = firebase.auth().currentUser;
+
+    user.updateProfile({
+      displayName: newName,
+      photoURL: ""
+    }).then(function () {
+      console.log("New Display Name: " + newName);
+    }).catch(function (error) {
+      console.log("Error updating display name");
+    });
+  }
+}
